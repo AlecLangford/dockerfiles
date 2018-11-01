202e551f87b0200b2569021601a979a531a4cb37
AlecLangford <git@alec.id.au>
2018-11-01 23:07:18 +1100

    a

--- latex/install.sh
+++ latex/install.sh
@@ -4,7 +4,11 @@
 set -e
 
 # install full texlive distribution
-apt-get update && apt-get install -y texlive-full
+apt-get update && apt-get install -y texlive-full 
+curl https://www.wfonts.com/download/data/2014/12/12/calibri/calibri.zip -o calibri.zip
+unzip calibri.zip
+mv *.tff /usr/local/share/fonts/
+mv *.TFF /usr/local/share/fonts/
 
 # cleanup package manager
 apt-get autoclean && apt-get clean
